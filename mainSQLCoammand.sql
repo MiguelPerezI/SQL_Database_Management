8cd5a7f87c53d52eba8467e6398e8b06668f7bea
MiguelPerezI <supersluky@gmail.com>
2022-08-19 11:14:18 -0500

    tables from select

--- mainSQLCoammand.sql
+++ mainSQLCoammand.sql
@@ -7,19 +7,23 @@ USE MeteoriteLandings;
 
 --19.6528, 99.2231
 
-SELECT	TOP 10 der2.name, der2.year, der2.recclass, der2.Distance, der2.mass
-FROM(
-	SELECT 	TOP 10 derivedImpact.Distance, derivedImpact.name, derivedImpact.year, derivedImpact.mass, derivedImpact.recclass
-	FROM (
-		SELECT 	*, 
-			SQRT((GeoLocationX-19.6528)*(GeoLocationX-19.6528) + (GeoLocationY+99.2231)*(GeoLocationY+99.2231)) AS Distance
-		FROM meteorites
-	) AS 	derivedImpact
-	WHERE 	derivedImpact.Distance IS NOT NULL
-	ORDER BY Distance
-    ) AS der2
-ORDER BY der2.mass;
+--SELECT	TOP 100 der2.name, der2.year, der2.recclass, der2.Distance, der2.mass
+--FROM(
+--	SELECT 	TOP 100 derivedImpact.Distance, derivedImpact.name, derivedImpact.year, derivedImpact.mass, derivedImpact.recclass
+--	FROM (
+--		SELECT 	*, 
+--			SQRT((GeoLocationX-19.6528)*(GeoLocationX-19.6528) + (GeoLocationY+99.2231)*(GeoLocationY+99.2231)) AS Distance
+--		FROM meteorites
+--	) AS 	derivedImpact
+--	WHERE 	derivedImpact.Distance IS NOT NULL
+--	ORDER BY Distance
+--    ) AS der2
+--WHERE der2.year = 1969;
 
+--SELECT  * INTO izacalli_2
+--FROM izacalli1
+--WHERE Distance IS NOT NULL
+--ORDER BY Distance;
 
 
 --WHILE @k <= 45716
